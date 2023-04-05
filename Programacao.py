5dbb025f6f1a0ad29c1f8fa3b1896ab20b9dc64a
Davidson Sousa <127262750+DavidsonSous@users.noreply.github.com>
2023-04-05 19:47:49 -0300

    Desenvolvendo um código básico em Python

---
+++ Programacao.py
@@ -0,0 +1,4 @@
+nome = input('Digite seu nome: ')
+idade = int(input('Digite sua idade: '))
+print(' ')
+saudacao = print(f'Olá, {nome}, tudo bem? Espero que sim. Vi aqui no seu cadastro que você tem {idade} anos, correto?')

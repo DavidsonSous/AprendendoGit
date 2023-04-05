97aaa79d2546a4d5be266fb8ec79a72303a066c0
Davidson Sousa <127262750+DavidsonSous@users.noreply.github.com>
2023-04-05 20:27:24 -0300

    Atualização e mais inputs

--- Programacao.py
+++ Programacao.py
@@ -1,4 +1,9 @@
 nome = input('Digite seu nome: ')
 idade = int(input('Digite sua idade: '))
+estado_civil = input('Digite seu estado civil atual: ')
+estado = input('Digite o estado: ')
+cidade = input('Digite a cidade: ')
+
 print(' ')
-saudacao = print(f'Olá, {nome}, tudo bem? Espero que sim. Vi aqui no seu cadastro que você tem {idade} anos, correto?')
+saudacao = print(f'Olá, {nome} Seja bem vindo (a) ao nosso formulário de pesquisa. Vi aqui no seu cadastro que você tem {idade} anos e seu estado civil é: {estado_civil}, você reside no estado de: {estado} e na cidade: {cidade}, correto?')
+print(' ')
